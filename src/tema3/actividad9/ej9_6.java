385c84f39033111752f96d398475a84a2cc622a8
aidan <aidsaegar@alu.edu.gva.es>
2024-10-22 08:48:11 +0200

    actividades 20241018

--- src/tema3/actividad9/ej9_6.java
+++ src/tema3/actividad9/ej9_6.java
@@ -8,6 +8,11 @@ public class ej9_6 {
         System.out.print("Número: ");
         return sc.nextInt();
     }
+    public static String pedirString() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Número: ");
+        return sc.nextLine();
+    }
     public static int voltea(int num) {
         int volteado = 0;
         int digito;
@@ -44,8 +49,56 @@ public class ej9_6 {
         suma /= 10;
         return voltea(suma);
     }
-    public static int binToHex (int bin) {
-        return 0;
+    public static String binToHex (int bin) {
+        int dec = binToDec(bin);
+        return (Integer.toHexString(dec).toUpperCase());
+    }
+    public static int decToBin (int dec) {
+        int bin= 0;
+        int factor = 1;
+
+        while (dec > 0) {
+            int resto = dec % 2; // Almacena el resto que sera el número binario
+            bin = bin + resto * factor;
+            factor *= 10;
+            dec = dec / 2;
+        }
+        return bin;
+    }
+    public static int decToOct (int dec) {
+        int resto, suma = 0;
+
+        while (dec > 0) {
+            resto = dec % 8;
+            suma = suma * 10 + resto;
+            dec /= 8;
+        }
+        return voltea(suma);
+    }
+    public static String decToHex (int dec) {
+        return (Integer.toHexString(dec).toUpperCase());
+    }
+    public static String octToBin (String oct) {
+        int dec = Integer.parseInt(oct, 8);
+        return Integer.toBinaryString(dec);
+    }
+    public static int octToDec (String oct) {
+        return Integer.parseInt(oct, 8);
+    }
+    public static String octToHex (String oct) {
+        int dec = Integer.parseInt(oct, 8);
+        return Integer.toHexString(dec).toUpperCase();
+    }
+    public static String hexToBin (String hex) {
+        int dec = Integer.parseInt(hex, 16);
+        return Integer.toBinaryString(dec);
+    }
+    public static int hexToDec (String hex) {
+        return Integer.parseInt(hex, 16);
+    }
+    public static String hexToOct (String hex) {
+        int dec = Integer.parseInt(hex, 16);
+        return Integer.toOctalString(dec);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -75,7 +128,34 @@ public class ej9_6 {
                             System.out.println("El número en octal es " + binToOct(pedirNum()));
                             break;
                         case 3:
-                            System.out.println("El número en hexadecimal " + binToHex(pedirNum()));
+                            System.out.println("El número en hexadecimal es " + binToHex(pedirNum()));
+                            break;
+                        case 4:
+                            System.out.println("El número en binario es " + decToBin(pedirNum()));
+                            break;
+                        case 5:
+                            System.out.println("El numero en octal es " + decToOct(pedirNum()));
+                            break;
+                        case 6:
+                            System.out.println("El numero en hexadecimal es " + decToHex(pedirNum()));
+                            break;
+                        case 7:
+                            System.out.println("El numero en binario es " + octToBin(pedirString()));
+                            break;
+                        case 8:
+                            System.out.println("El numero en decimal es " + octToDec(pedirString()));
+                            break;
+                        case 9:
+                            System.out.println("El numero en hexadecimal es " + octToHex(pedirString()));
+                            break;
+                        case 10:
+                            System.out.println("El numero en binario es " + hexToBin(pedirString()));
+                            break;
+                        case 11:
+                            System.out.println("El numero en decimal es " + hexToDec(pedirString()));
+                            break;
+                        case 12:
+                            System.out.println("El numero en octal es " + hexToOct(pedirString()));
                     }
                 } else System.out.println("Opción no valida");
                 correcto = true;
