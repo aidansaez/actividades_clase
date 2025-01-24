07d9f836b7635ec7888af2c8d4fabf21e8e6d9ca
aidsaegar <aidsaegar@alu.edu.gva.es>
2025-01-24 09:05:52 +0100

    20250124

---
+++ src/tema6/actividad2/ejercicio4/Coche.java
@@ -0,0 +1,5 @@
+package tema6.actividad2.ejercicio4;
+
+public class Coche {
+
+}
---
+++ src/tema6/actividad2/ejercicio4/MainPolimorfismo.java
@@ -0,0 +1,7 @@
+package tema6.actividad2.ejercicio4;
+
+public class MainPolimorfismo {
+    public static void main(String[] args) {
+
+    }
+}
