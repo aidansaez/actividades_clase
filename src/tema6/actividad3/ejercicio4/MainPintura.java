24ec0e67089d3154df22cb8db2411b2b74b3747f
aidsaegar <aidsaegar@alu.edu.gva.es>
2025-01-27 08:21:06 +0100

    20250124

--- src/tema6/actividad3/ejercicio4/MainPintura.java
+++ src/tema6/actividad3/ejercicio4/MainPintura.java
@@ -4,9 +4,9 @@ public class MainPintura {
     public static void main(String[] args) {
         Pintura pintura = new Pintura(250);
 
-        Rectangulo rectangulo = new Rectangulo(20, 35);
-        Esfera esfera = new Esfera(15);
-        Cilindro cilindro = new Cilindro(10, 30);
+        Forma rectangulo = new Rectangulo(20, 35);
+        Forma esfera = new Esfera(15);
+        Forma cilindro = new Cilindro(10, 30);
 
         pintura.PinturaNecesaria(rectangulo);
         pintura.PinturaNecesaria(esfera);
