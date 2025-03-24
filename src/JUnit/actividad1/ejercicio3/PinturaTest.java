03ef6a7cf3002a06dea5c3fe90c5f77eb8659a25
aidan <aidsaegar@alu.edu.gva.es>
2025-03-24 07:19:22 +0100

    actividades 20250324

--- src/JUnit/actividad1/ejercicio3/PinturaTest.java
+++ src/JUnit/actividad1/ejercicio3/PinturaTest.java
@@ -6,20 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PinturaTest {
     @Test
-    void TestToString() {
-        assertEquals("nombre=Esfera", new Esfera(3).toString());
-        assertEquals("nombre=Rectangulo", new Rectangulo(3, 5).toString());
-        assertEquals("nombre=Cilindro", new Cilindro(3, 5).toString());
-    }
-
-    @Test
-    void TestDevolverArea() {
-        assertEquals(4*Math.PI*3*3, new Esfera(3).area(), 0.1);
-        assertEquals(15, new Rectangulo(3,5).area());
-        assertEquals(Math.PI*3*3*5, new Cilindro(3,5).area(), 0.001);
-    }
-
-    @Test
     void TestPinturaNecesaria() {
         Rectangulo rectangulo = new Rectangulo(3,6);
         assertEquals(9, new Pintura(2).PinturaNecesaria(rectangulo));
