83a118f3801f12e37b5d445dbc85e1f35e8409d5
aidan <aidsaegar@alu.edu.gva.es>
2025-03-31 08:33:56 +0200

    actividades 20250331

---
+++ src/Ficheros/JugadoresAlojados.java
@@ -0,0 +1,29 @@
+package Ficheros;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class JugadoresAlojados {
+    public static void main(String[] args) {
+        File f = new File("C:/ficheros/jogadors.txt");
+        File fA = new File("C:/ficheros/alojados.txt");
+
+        try (Scanner sc = new Scanner(f)) {
+            PrintWriter pw = new PrintWriter(fA);
+
+            while (sc.hasNextLine()) {
+                String linea = sc.nextLine();
+                String[] partes = linea.split(";");
+                if (linea.contains("H")) {
+                    System.out.println(partes[0] + " - " + partes[2]);
+                    pw.println(partes[0] + " - " + partes[2]);
+                }
+            }
+            pw.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
